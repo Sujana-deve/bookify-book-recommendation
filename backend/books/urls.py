0eb55e3844978406f031a2850ce12491166bd613
sujana <sharmasujana420@gmail.com>
2026-08-03 21:38:13 +0545

    added the endpoints path

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('health/', views.health_check, name='health-check'),
     path('<int:book_id>/recommendations/', views.recommendations, name='recommendations'),
+    path('<int:book_id>/recommendations/cf/', views.item_based_recommendations, name='cf-item-recommendations'),
+    path('users/<int:user_id>/recommendations/', views.user_based_recommendations, name='cf-user-recommendations'),
     path('<int:pk>/read/', views.BookReaderView.as_view()),
 ]
